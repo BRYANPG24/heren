ea1bbdd9d5b56fd2767c8d7e478abde7a443fea7
BRYANPG24 <145297802+BRYANPG24@users.noreply.github.com>
2023-09-29 20:36:41 -0500

    Update inde.ts

---
+++ Herencia/inde.ts
@@ -0,0 +1,55 @@
+const persona1 = new Personalservicio();
+persona1.name = "Angel";
+persona1.apellido = "Mora";
+persona1.numeroidentificacion = "1555646789";
+persona1.estadocivil = "Soltero";
+persona1.imprimirInformacion();
+
+persona1.cambiodeestado("Casado");
+persona1.imprimirInformacion();
+
+const Estudiante1 = new estudiantes();
+Estudiante1.name = "Ana";
+Estudiante1.apellido = "Lopez";
+Estudiante1.numeroidentificacion = "987654321";
+Estudiante1.estadocivil = "Soltera";
+Estudiante1.curso= "Cálculo";
+Estudiante1.imprimirInformacion();
+
+Estudiante.matriculaNuevoCurso("Álgebra");
+Estudiante.imprimirInformacion();
+
+const profesor1 = new Profesores();
+profesor1.name = "Pedro";
+profesor1.apellido = "Lucas";
+persona1.numeroidentificacion = "786756123";
+profesor1.estadocivil = "Casado";
+profesor1.departamento = "Matematicas";
+
+profesor1.cambiarDepartamento("fisica general");
+profesor1.imprimirInformacion();
+
+const Empleados1 = new empleados();
+Empleados1.name = "Carlos";
+Empleados1.apellido = "Lopez";
+Empleados1.numeroidentificacion = "789123456";
+Empleados1.estadocivil = "Soltero";
+Empleados1.anoincorporacion = 2015;
+Empleados1.numerodespacho = "D-201";
+
+
+Empleados1.Reasignaciondespacho("C-123")
+Empleados1.imprimirInformacion();
+
+const PersonalServicio1 = new personalservicio();
+PersonalServicio1.name = "Anne";
+PersonalServicio1.apellido = "Moreira";
+PersonalServicio1.numeroidentificacion = "654321987"
+PersonalServicio1.estadocivil = "casada";
+PersonalServicio1.anoincorporacion = 2018;
+PersonalServicio1.numerodespacho = "A-101";
+PersonalServicio1.seccion = "Biblioteca";
+
+
+PersonalServicio1.transladarsección("Decanato");
+PersonalServicio1.imprimirInformacion();
